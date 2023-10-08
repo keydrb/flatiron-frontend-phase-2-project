8c153783e551cac5d0c9017c025814685bd9f7f1
Keydra Burtin <bkeydra@gmail.com>
2023-10-07 22:25:22 -0500

    wip project

--- todo-list-app/src/components/navbar.js
+++ todo-list-app/src/components/navbar.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+      <nav>
+        <ul>
+          <li><Link to="/">Task List</Link></li>
+          <li><Link to="/add">Add Task</Link></li>
