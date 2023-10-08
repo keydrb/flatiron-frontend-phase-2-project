da0f6f9020c91f0aace5a1fbd080f8755e9503ce
Keydra Burtin <bkeydra@gmail.com>
2023-10-07 21:44:11 -0500

    wip project

---
+++ todo-list-app/src/components/tasklist.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const TaskList = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    // Fetch tasks from json-server
+    axios.get('http://localhost:3001/tasks')
+      .then(response => setTasks(response.data))
+      .catch(error => console.error('Error fetching tasks:', error));
+  }, []);
+
+  return (
+    <div>
+      <h2>Task List</h2>
+      <ul>
+        {tasks.map(task => (
+          <li key={task.id}>{task.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TaskList;
