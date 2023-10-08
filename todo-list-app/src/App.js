89edd95f80aa222c769e43a1cec21827a0e431fe
Keydra Burtin <bkeydra@gmail.com>
2023-10-07 22:32:35 -0500

    wip project

--- todo-list-app/src/App.js
+++ todo-list-app/src/App.js
@@ -3,3 +3,10 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/navbar';
 import TaskList from './components/tasklist';
 import AddTask from './components/AddTask';
+
+function App() {
+  const [tasks, setTasks] = React.useState([]);
+
+  const handleTaskAdded = (newTask) => {
+    setTasks((prevTasks) => [...prevTasks, newTask]);
+  };
