4479cdbfcfdecee1ef1b155295c7e4486a6cdbea
Keydra Burtin <bkeydra@gmail.com>
2023-10-08 03:42:53 -0500

    localhost test

--- todo-list-app/src/App.js
+++ todo-list-app/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
+      <h1>Simple To-do list</h1>
     </div>
   );
 }
